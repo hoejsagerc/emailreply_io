45691595ad78870c36c5ad37aa19632e45998dd1
Christian Højsager <chris@scriptingchris.tech>
2023-01-08 18:30:41 +0100

    initial commit

---
+++ EmailReplyIo/Models/PromptRequest.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EmailReplyIo.Models
+{
+    public class PromptRequest
+    {
+        public string model { get; set; }
+
+        [Required, StringLength(700)]
+        public string prompt { get; set; }
+
+        public int temperature { get; set; }
+
+        public int max_tokens { get; set; }
+
+        public PromptRequest()
+        {
+            this.model = "text-davinci-002";
+
+            this.prompt = "Write this is a test";
+
+            this.temperature = 0;
+
+            this.max_tokens = 500;
+        }
+    }
+}
---
+++ EmailReplyIo/Models/PromptResponse.cs
@@ -0,0 +1,27 @@
+﻿namespace EmailReplyIo.Models
+{
+    public class PromptResponse
+    {
+        public string? Id { get; set; }
+        public string? _object { get; set; }
+        public int Created { get; set; }
+        public string? Model { get; set; }
+        public Choice[]? Choices { get; set; }
+        public Usage? Usage { get; set; }
+    }
+
+    public class Usage
+    {
+        public int Prompt_tokens { get; set; }
+        public int Completion_tokens { get; set; }
+        public int Total_tokens { get; set; }
+    }
+
+    public class Choice
+    {
+        public string? Text { get; set; }
+        public int Index { get; set; }
+        public object? Logprobs { get; set; }
+        public string? Finish_reason { get; set; }
+    }
+}
